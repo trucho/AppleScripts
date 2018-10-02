bde5561255f218a6c02448e51bda6752df08826e
Trucho <angueyra@gmail.com>
2018-10-02 17:38:33 -0400

    added my bashprolife
    remember to source this an temrinal startup

---
+++ trucho_bashprofile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# added by Anaconda3 5.2.0 installer
+export PATH="~/anaconda3/bin:$PATH"
+alias jupyter="~/anaconda3/bin/jupyter lab --NotebookApp.notebook_dir=~/Documents/Python/PythonCode/"
