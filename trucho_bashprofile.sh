c6c8ad4ac714e0a1d4445f53f895fc03403bf4d0
trucho <trucho@users.noreply.github.com>
2022-05-26 09:59:53 -0400

    Update trucho_bashprofile.sh

--- trucho_bashprofile.sh
+++ trucho_bashprofile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # added by Anaconda3 5.2.0 installer
-export PATH="~/anaconda3/bin:$PATH"
-alias jupyter="~/anaconda3/bin/jupyter lab --NotebookApp.notebook_dir=~/Documents/Python/PythonCode/"
+export PATH="$HOME/opt/anaconda3/bin:$PATH"
+alias jupyter="~/opt/anaconda3/bin/jupyter lab --NotebookApp.notebook_dir=~/PythonCode/"
